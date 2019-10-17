8d7f9624fa47c3b0d5563c29f4ed38d98f99e443
saivishalvangala <saivishalvangala@gmail.com>
2019-10-17 19:35:40 +0530

    task 8-Using application.properties file to get values of fields

--- src/main/java/com/stackroute/muzixApp/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/muzixApp/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.stackroute.muzixApp.swaggerConfig;
+package com.stackroute.muzixApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/stackroute/muzixApp/config/WebConfiguration.java
+++ src/main/java/com/stackroute/muzixApp/config/WebConfiguration.java
@@ -5,13 +5,21 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class WebConfiguration {
     @Bean
     ServletRegistrationBean h2servletRegistration(){
         ServletRegistrationBean registrationBean= new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
+
+    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
